4848bf202232fbd8008dbeeccfc455d1b98d3ab0
Nooshin <2011dragonmails@gmail.com>
2024-01-02 00:45:49 +0330

    working on address form - mobile mode

---
+++ src/components/address/AddressForm.js
@@ -0,0 +1,102 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import classes from "./AddressForm.module.scss";
+
+const AddressForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data, event) => {
+    event.preventDefault();
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className={classes["form"]}>
+        <button type="button" className={classes['close-btn']}>
+          <svg>
+            <use href="/icons.svg#svg-close_form" />
+          </svg>
+          <span>
+            Add New Address
+          </span>
+        </button>
+        <div className={`${classes["form__section"]} ${classes["form--text"]}`}>
+          <h3 className={classes["section__title"]}>Contact Information</h3>
+          <div className={classes["input__container"]}>
+            <label htmlFor="fullName">Full Name</label>
+            <input
+              id="fullName"
+              type="text"
+              placeholder="Full Name"
+              {...register("fullName", { required: true })}
+            />
+          </div>
+          <div className={classes["input__container"]}>
+            <label htmlFor="mobileNumber">Mobile Number</label>
+            <input
+              id="mobileNumber"
+              type="text"
+              placeholder="Enter Number"
+              {...register("mobileNumber", {
+                required: true,
+                pattern: /^[0-9]*$/,
+              })}
+            />
+          </div>
+        </div>
+        <div className={`${classes["form__section"]} ${classes["form--text"]}`}>
+          <h3 className={classes["section__title"]}>Delivery Address</h3>
+          <div className={classes["input__container"]}>
+            <label htmlFor="street">Street Address</label>
+            <input
+              id="street"
+              type="text"
+              placeholder="Enter Address"
+              {...register("street", { required: true })}
+            />
+          </div>
+          <div className={classes["input__container"]}>
+            <label htmlFor="state">State</label>
+            <input
+              id="state"
+              type="text"
+              placeholder="Enter State"
+              {...register("state", { required: true })}
+            />
+          </div>
+          <div className={classes["input__container"]}>
+            <label htmlFor="city">City</label>
+            <input
+              id="city"
+              type="text"
+              placeholder="Enter City"
+              {...register("city", { required: true })}
+            />
+          </div>
+          <div className={classes["input__container"]}>
+            <label htmlFor="pin">Pin Code</label>
+            <input
+              id="pin"
+              type="text"
+              placeholder="Enter Pin Code"
+              {...register("pin", { required: true })}
+            />
+          </div>
+        </div>
+        <input
+          type="submit"
+          value="Save Address"
+          className={classes["submit-btn"]}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default AddressForm;
--- src/components/checkout/FullCheckout.js
+++ src/components/checkout/FullCheckout.js
@@ -1,11 +1,13 @@
 import OrderSummary from "./OrderSummary";
 import classes from './FullCheckout.module.scss'
+import AddressForm from "../address/AddressForm";
 
 const FullCheckout = (props) => {
   return (
     <div className={classes['padding-inline-default']}>
       <h1 className={classes['checkout__title']}>Checkout</h1>
       <OrderSummary cart = {props.cart} />
+      <AddressForm />
     </div>
   );
 };
